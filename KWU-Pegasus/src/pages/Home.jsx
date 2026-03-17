e7e2f0372dbbb20245b2753a02931c2e079b5bbb
jundragon7419 <jundragon7419@gmail.com>
2026-03-17 18:26:30 +0900

    작업 저장

--- KWU-Pegasus/src/pages/Home.jsx
+++ KWU-Pegasus/src/pages/Home.jsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <section className={styles.home}>
       <h2 className={styles.title}>홈 화면 더미 페이지</h2>
-      <p className={styles.description}>
-        이곳이 홈 화면입니다. 나중에 실제 콘텐츠와 컴포넌트를 추가해주세요.
-      </p>
     </section>
   )
 }
